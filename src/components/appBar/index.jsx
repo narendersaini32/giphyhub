6767237a681e8b99f3d9d7aa4563fc945b61d024
narender saini <narendersaini32@gmail.com>
2019-04-20 14:55:12 +0530

    API Key is saved

--- src/components/appBar/index.jsx
+++ src/components/appBar/index.jsx
@@ -1,7 +1,10 @@
 import React, { PureComponent, Fragment } from 'react';
 import Styled from 'styled-components';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Typography from '@material-ui/core/Typography';
 import AppBarComponent from './appBarRender';
 import GifCard from '../gifCard';
+import { giphyApiKey } from '../../keys.json';
 
 const Wrapper = Styled.div`
 display: flex;
@@ -15,25 +18,35 @@ class AppBar extends PureComponent {
   }
 
   onSearch = text => {
+    this.setState({ isLoading: true });
     //  eslint-disable-next-line
-    fetch(
-      `http://api.giphy.com/v1/gifs/search?q=${text}&api_key=FyIGrVHWHbZIJHSwBsiGZat60OwKsqGs&limit=5`
-    )
+    fetch(`http://api.giphy.com/v1/gifs/search?q=${text}&api_key=${giphyApiKey}&limit=10`)
       .then(res => res.json())
       .then(
         result => {
           const { data = [] } = result;
-          this.setState({ data });
+          this.setState({ data, isLoading: false, error: '' });
         },
-        error => console.log('TCL: AppBar -> error', error)
+        error => {
+          console.log('TCL: AppBar -> error', error);
+
+          this.setState({ isLoading: false, error });
+        }
       );
   };
 
   render() {
-    const { data = [] } = this.state;
+    const { data = [], isLoading, error } = this.state;
     return (
       <Fragment>
         <AppBarComponent onSearch={this.onSearch} />
+        {isLoading && <LinearProgress />}
+        {error && (
+          <Typography component="h2" variant="h1" gutterBottom>
+            {error}
+          </Typography>
+        )}
+
         <Wrapper>
           {data.map(obj => {
             const {
@@ -41,7 +54,7 @@ class AppBar extends PureComponent {
                 downsized: { url }
               }
             } = obj;
-            return <GifCard url={url} />;
+            return <GifCard url={url} key={url} />;
           })}
         </Wrapper>
       </Fragment>
